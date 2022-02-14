abadc6997127ef6e6b59f810cddb14234ef9dfda
Tim Stemke <djdtimit@aol.com>
2022-02-14 18:22:42 +0100

    added paths to data lake

---
+++ h-and-m-personalized-fashion-recommendations/utils/util.py
@@ -0,0 +1,11 @@
+def ingestion_path():
+    return "abfss://playground@sadatabrickststemke.dfs.core.windows.net/ingestion/h-and-m-personalized-fashion-recommendations/"
+
+def raw_path():
+    return "abfss://playground@sadatabrickststemke.dfs.core.windows.net/raw/h-and-m-personalized-fashion-recommendations/"
+
+def qualified_path():
+    return "abfss://playground@sadatabrickststemke.dfs.core.windows.net/qualified/h-and-m-personalized-fashion-recommendations/"
+
+def curated_path():
+    return "abfss://playground@sadatabrickststemke.dfs.core.windows.net/curated/h-and-m-personalized-fashion-recommendations/"
