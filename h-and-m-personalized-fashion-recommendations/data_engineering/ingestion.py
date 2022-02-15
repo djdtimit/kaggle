b7ff3a1b81a659d67b6efa660065efedafabbc96
djdtimit@aol.com <djdtimit@aol.com>
2022-02-15 09:00:22 +0000

    added commands for writing from ingestion to raw

--- h-and-m-personalized-fashion-recommendations/data_engineering/ingestion.py
+++ h-and-m-personalized-fashion-recommendations/data_engineering/ingestion.py
@@ -1,2 +1,31 @@
 # Databricks notebook source
+from utils.lineage import add_lineage
+from utils.paths import *
+
+# COMMAND ----------
+
+add_lineage(spark.read.csv(ingestion_path() + 'articles/', header = True)).write.save(raw_path() + 'articles/')
+
+# COMMAND ----------
+
+add_lineage(spark.read.csv(ingestion_path() + 'customers/', header = True)).write.save(raw_path() + 'customers/')
+
+# COMMAND ----------
+
+add_lineage(spark.read.csv(ingestion_path() + 'transactions/', header = True)).write.save(raw_path() + 'transactions/')
+
+# COMMAND ----------
+
+add_lineage(spark.read.format("image").load(ingestion_path() + 'images/*/').write.save(raw_path() + 'images/')
+
+# COMMAND ----------
+
+# df = spark.read.format("image").load(ingestion_path() + 'images/*/')
+
+# COMMAND ----------
+
+# df.display()
+
+# COMMAND ----------
+
 
