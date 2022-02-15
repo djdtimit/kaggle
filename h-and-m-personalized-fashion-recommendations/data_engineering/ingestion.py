fc56b122d2e0d0e66cfcc3a59ee14053b3f20648
djdtimit@aol.com <djdtimit@aol.com>
2022-02-15 20:58:15 +0000

    wrote image data from ingestion to raw

--- h-and-m-personalized-fashion-recommendations/data_engineering/ingestion.py
+++ h-and-m-personalized-fashion-recommendations/data_engineering/ingestion.py
@@ -1,6 +1,26 @@
 # Databricks notebook source
 from utils.lineage import add_lineage
 from utils.paths import *
+# from utils.images import *
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+import io
+from PIL import Image
+import pandas as pd
+from pyspark.sql.functions import pandas_udf
+
+
+def extract_size(content):
+  """Extract image size from its raw content."""
+  image = Image.open(io.BytesIO(content))
+  return image.size
+ 
+@pandas_udf("width: int, height: int")
+def extract_size_udf(content_series):
+  sizes = content_series.apply(extract_size)
+  return pd.DataFrame(list(sizes))
 
 # COMMAND ----------
 
@@ -16,4 +36,11 @@ add_lineage(spark.read.csv(ingestion_path() + 'transactions/', header = True)).w
 
 # COMMAND ----------
 
-add_lineage(spark.read.format("image").load(ingestion_path() + 'images/*/')).write.save(raw_path() + 'images/')
+# add_lineage(spark.read.format("image").load(ingestion_path() + 'images/*/')).write.save(raw_path() + 'images/')
+
+# COMMAND ----------
+
+spark.read.format("binaryFile").load(ingestion_path() + 'images/*/').select(
+  col("path"),
+  extract_size_udf(col("content")).alias("size"),
+  col("content")).write.save(raw_path() + 'images/')
